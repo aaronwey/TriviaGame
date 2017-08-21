ee4bbbb3e022446d2fb5dd223ef177c3b1b15f18
aaronwey <aaronwey@gmail.com>
2017-08-21 17:21:35 -0600

    3rd commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
 // Create a function that creates the start button and initial screen
 
 function initialScreen() {
-	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	startScreen = 
+	"<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
 	$(".mainArea").html(startScreen);
 }
 
 initialScreen();
 
-//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+//Create a function, generateHTML(), that is triggered by the start button
 
 $("body").on("click", ".start-button", function(event){
-	event.preventDefault();  // added line to test issue on GitHub Viewer
-	clickSound.play();
+	// event.preventDefault();  // added line to test issue on GitHub Viewer
+
 	generateHTML();
 
 	timerWrapper();
@@ -21,7 +22,7 @@ $("body").on("click", ".start-button", function(event){
 
 $("body").on("click", ".answer", function(event){
 	//answeredQuestion = true;
-	clickSound.play();
+
 	selectedAnswer = $(this).text();
 	if(selectedAnswer === correctAnswers[questionCounter]) {
 		//alert("correct");
@@ -37,7 +38,7 @@ $("body").on("click", ".answer", function(event){
 }); // Close .answer click
 
 $("body").on("click", ".reset-button", function(event){
-	clickSound.play();
+
 	resetGame();
 }); // Closes reset-button click
 
@@ -45,32 +46,41 @@ $("body").on("click", ".reset-button", function(event){
 
 function generateLossDueToTimeOut() {
 	unansweredTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	gameHTML = 
+	"<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
+	"<p class='text-center'>No more time you dum dum!  The correct answer was: " + correctAnswers[questionCounter] +
+	"</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 4000);  //  change to 4000 or other amount
+	setTimeout(wait, 4000);  
 }
 
 function generateWin() {
 	correctTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
+	"<p class='text-center'>Got it Smarty Pants! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 4000);  //  change to 4000 or other amount
+	setTimeout(wait, 4000);    
 }
 
 function generateLoss() {
 	incorrectTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
+	"<p class='text-center'>Bad Job! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + 
+	"<img class='center-block img-wrong' src='img/x.png'>";
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 4000); //  change to 4000 or other amount
+	setTimeout(wait, 4000); 
 }
 
 function generateHTML() {
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>"
+	 + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + 
+	 "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+
+	 "</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
 	$(".mainArea").html(gameHTML);
 }
 
 function wait() {
-	if (questionCounter < 7) {
+	if (questionCounter < 5) {
 	questionCounter++;
 	generateHTML();
 	counter = 30;
@@ -113,14 +123,23 @@ function resetGame() {
 var startScreen;
 var gameHTML;
 var counter = 30;
-var questionArray = ["What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
-var answerArray = [["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
-var imageArray = ["<img class='center-block img-right' src='assets/images/australia.png'>", "<img class='center-block img-right' src='assets/images/liberia.png'>", "<img class='center-block img-right' src='assets/images/taiwan.png'>", "<img class='center-block img-right' src='assets/images/japan.png'>", "<img class='center-block img-right' src='assets/images/china.png'>", "<img class='center-block img-right' src='assets/images/turkey.png'>", "<img class='center-block img-right' src='assets/images/colombia.png'>", "<img class='center-block img-right' src='assets/images/india.png'>"];
-var correctAnswers = ["A. Canberra", "B. Monrovia", "C. Taipei", "C. Tokyo", "D. Beijing", "A. Ankara", "B. Bogota", "D. New Delhi"];
+var questionArray = ["Which is the most abundant metal in the earth's crust?", "What is the second most abundant element in the earth's atmosphere?",
+					 "What is the world's largest ocean?", "In what month is the Earth closest to the sun?", 
+					 "What is the only sea on Earth with no coastline?", "Which Country has the most trees?"];
+
+var answerArray = [["Aluminum", "Copper", "Iron", "Gold"], ["Hydrogen","Oxygen","Argon","Carbon"], ["Atlantic", "Indian", "Pacific", "Arctic"], 
+				   ["June","September","January","April"], ["Mediterranean", "Baltic", "Black", "Sargasso"], 
+				   ["Russia","United States","Canada","Brazil"]];
+
+var imageArray = ["<img class='center-block img-right' src='Aluminum'>", "<img class='center-block img-right' src='oxygen'>", 
+					"<img class='center-block img-right' src='Pacific'>", "<img class='center-block img-right' src='New years'>", 
+					"<img class='center-block img-right' src='Sargasso'>", "<img class='center-block img-right' src='Russia'>",];
+
+var correctAnswers = ["A. Aluminum", "B. Oxygen", "C. Pacific", "C. January", "D. Sargasso", "A. Russia"];
 var questionCounter = 0;
 var selecterAnswer;
 var theClock;
 var correctTally = 0;
 var incorrectTally = 0;
 var unansweredTally = 0;
-var clickSound = new Audio("sound/button-click.mp3");
+
