7bd16716978c8bcb301863a8d0e3b67e99716a36
aaronwey <aaronwey@gmail.com>
2017-08-21 19:02:54 -0600

    5th commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,69 @@
 $(document).ready(function() {
-// Create a function that creates the start button and initial screen
+
 
 function initialScreen() {
-	startScreen = 
-	"<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
 	$(".mainArea").html(startScreen);
 }
 
 initialScreen();
 
-//Create a function, generateHTML(), that is triggered by the start button
+
 
 $("body").on("click", ".start-button", function(event){
-	// event.preventDefault();  // added line to test issue on GitHub Viewer
-
 	generateHTML();
-
 	timerWrapper();
 
-}); // Closes start-button click
+}); 
 
 $("body").on("click", ".answer", function(event){
-	//answeredQuestion = true;
-
+	
 	selectedAnswer = $(this).text();
-	if(selectedAnswer === correctAnswers[questionCounter]) {
-		//alert("correct");
-
+	if(selectedAnswer === correctAnswers[questionCounter]) {		
 		clearInterval(theClock);
 		generateWin();
 	}
 	else {
-		//alert("wrong answer!");
 		clearInterval(theClock);
 		generateLoss();
 	}
-}); // Close .answer click
+}); 
 
 $("body").on("click", ".reset-button", function(event){
-
 	resetGame();
-}); // Closes reset-button click
+}); 
 
-});  //  Closes jQuery wrapper
+});  
 
 function generateLossDueToTimeOut() {
 	unansweredTally++;
-	gameHTML = 
-	"<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
-	"<p class='text-center'>No more time you dum dum!  The correct answer was: " + correctAnswers[questionCounter] +
-	"</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" +
+	 "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + 
+	 "</p>" + "<img class='center-block img-wrong' src='assets/images/chicken.jpg'>";
 	$(".mainArea").html(gameHTML);
 	setTimeout(wait, 4000);  
 }
 
 function generateWin() {
 	correctTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
-	"<p class='text-center'>Got it Smarty Pants! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" +
+	 "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
 	$(".mainArea").html(gameHTML);
-	setTimeout(wait, 4000);    
+	setTimeout(wait, 4000);  
 }
 
 function generateLoss() {
 	incorrectTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + 
-	"<p class='text-center'>Bad Job! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + 
-	"<img class='center-block img-wrong' src='img/x.png'>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" +
+	 "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/chicken.jpg'>";
 	$(".mainArea").html(gameHTML);
 	setTimeout(wait, 4000); 
 }
 
 function generateHTML() {
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>"
-	 + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + 
-	 "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+
-	 "</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" +
+	 questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+
+	 answerArray[questionCounter][1] + "</p><p class='answer'>C. " + answerArray[questionCounter][2] + "</p><p class='answer'>D. " + answerArray[questionCounter][3] + "</p>";
 	$(".mainArea").html(gameHTML);
 }
 
@@ -106,7 +94,9 @@ function timerWrapper() {
 }
 
 function finalScreen() {
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + 
+	"</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + 
+	"</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
 	$(".mainArea").html(gameHTML);
 }
 
@@ -131,9 +121,9 @@ var answerArray = [["Aluminum", "Copper", "Iron", "Gold"], ["Hydrogen","Oxygen",
 				   ["June","September","January","April"], ["Mediterranean", "Baltic", "Black", "Sargasso"], 
 				   ["Russia","United States","Canada","Brazil"]];
 
-var imageArray = ["<img class='center-block img-right' src='Aluminum'>", "<img class='center-block img-right' src='oxygen'>", 
-					"<img class='center-block img-right' src='Pacific'>", "<img class='center-block img-right' src='New years'>", 
-					"<img class='center-block img-right' src='Sargasso'>", "<img class='center-block img-right' src='Russia'>",];
+var imageArray = ["<img class='center-block img-right' src='assets/images/aluminum.jpg'>", "<img class='center-block img-right' src='assets/images/oxygen.png'>", 
+					"<img class='center-block img-right' src='assets/images/pacific.png'>", "<img class='center-block img-right' src='assets/images/january.jpg'>", 
+					"<img class='center-block img-right' src='assets/images/sargasso.gif'>", "<img class='center-block img-right' src='assets/images/russia.jpg'>",];
 
 var correctAnswers = ["A. Aluminum", "B. Oxygen", "C. Pacific", "C. January", "D. Sargasso", "A. Russia"];
 var questionCounter = 0;
@@ -142,4 +132,3 @@ var theClock;
 var correctTally = 0;
 var incorrectTally = 0;
 var unansweredTally = 0;
-
